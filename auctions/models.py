ff95d62086843ec0230fecd33b146051bd543e0b
TolokMik <mikola.tolok@gmail.com>
2021-12-22 00:20:40 +0200

    comm1

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,38 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import CASCADE, SET_NULL
+from django.utils.html import format_html
+
 
 
 class User(AbstractUser):
-    pass
+    users_photo = models.ImageField()
+
+def custom_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = '{}.{}'.format(uuid.uuid4().hex[:10], ext)
+    return filename    
+
+class Staff(models.Model):
+    staffname = models.CharField(max_length = 64)
+    image_one = models.ImageField(upload_to='staff/',default='staff/F-208_H.png',blank=True,null=True)
+    staff_descript = models.TextField()
+    startprice = models.FloatField(null = True, blank=True)
+    staff_owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    def __str__(self):
+        return f"{self.staffname} | {self.startprice} | {self.staff_owner}"
+
+    def image_data(self):
+        return format_html(
+            '<img src="{}" width ="100px"/>',
+            self.image_one.url,
+        )
+    
+
+class Propositions(models.Model):
+    person_id = models.ForeignKey(User, on_delete = models.CASCADE, related_name="person")
+    staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE, related_name="staff")
+    price = models.FloatField()
+
+
+
--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,26 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, models
 from django.db import IntegrityError
+from django.db.models.fields import FloatField
+#from django.forms.models import _Labels
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Staff, Propositions
+from django import forms
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    staff = Staff.objects.get(id=3)
+    #staff.startprice =1000
+    #staff.save()
+    staff2 = Staff.objects.get(id=4)
+    #staff2.startprice =899
+    #staff2.save()
+    
+    return render(request, "auctions/index.html",{
+        "staff": [staff, staff2]
+    })
 
 
 def login_view(request):
@@ -61,3 +73,29 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+class Staff_Form(forms.Form):
+    staff_name = forms.CharField(label="Staff name", max_length=64)
+    staff_descript = forms.CharField(label="Description", max_length=200)
+    start_price = forms.FloatField(label='Start price')
+    staff_photo = forms.ImageField(label="Add staff photo")
+
+
+def add_listing(request):
+    if request.method == "POST":
+        staff_form = Staff_Form(request.POST, request.FILES)
+        if staff_form.is_valid():
+            staff_name = staff_form.cleaned_data["staff_name"]
+            staff_descript = staff_form.cleaned_data['staff_descript']
+            start_price = staff_form.cleaned_data['start_price']
+            staff_photo = staff_form.cleaned_data['staff_photo']
+            current_user = request.user
+            new_staff = Staff.objects.create(staffname=staff_name, staff_descript=staff_descript, startprice=start_price, image_one=staff_photo, staff_owner=current_user.id )
+            new_staff.save()
+    else:
+        staff_form = Staff_Form()
+      #  staff_form.staff_name.label="new staff name"
+    return render(request, "auctions/add_listing.html", { 
+        "listingform": staff_form
+    })
+
